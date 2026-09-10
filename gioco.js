bd74a7026029d3b8499243030e31b3d5a7437402
Tomoe-livic <paolo702001@gmail.com>
2026-09-11 01:06:55 +0200

    Aggiunte pagine, sistemato css, dato forma e statistiche alle classi, calcolo xp/livello, due mostraStat distinti per scopi diversi

--- gioco.js
+++ gioco.js
@@ -1,41 +1,172 @@
-//mago, guerriero, chierico(?), bardo, ladro
-let task = JSON.parse(localStorage.getItem("task")) || [
-    { id: 1, nome: "Studia", fatto: false, xp: 10 },
-    { id: 2, nome: "Allenati", fatto: false, xp: 10 },
-    { id: 3, nome: "Leggi", fatto: false, xp: 10 },
-    { id: 4, nome: "Suona", fatto: false, xp: 10 },
-    { id: 5, nome: "Crea", fatto: false, xp: 10 }
-];
-
-
-let giocatore = JSON.parse(localStorage.getItem("giocatore")) || {
-    xp: 0,
-    livello: 1
-};
-
+const formPersonaggio = document.querySelector("#creazione-personaggio");
+const inputNome = document.querySelector("#nome-giocatore");
+const sceltaClasse = document.querySelector ("#scelta-classe");
+const home = document.querySelector("#home");
+const incipit = document.querySelector("#incipit");
+const scelta2 = document.querySelector("#scelta2")
 const listaTask = document.querySelector("#lista-task");
 const inputTask = document.querySelector("#nuova-task");
 const bottoneAggiungi = document.querySelector("#aggiungi-task");
 const stat = document.querySelector("#stat");
+const tabBar = document.querySelector("#tab-bar");
+const paginaQuest = document.querySelector("#pagina-quest");
+const paginaStatistiche = document.querySelector("#pagina-statistiche");
 
+
+
+
+formPersonaggio.addEventListener("submit", (evento) => {
+    evento.preventDefault();
+    const nome = inputNome.value.trim();
+    giocatore.nome = nome;
+    salvaGiocatore();
+
+    formPersonaggio.hidden = true;
+    scelta2.hidden = false;
+})
+
+//mago, guerriero, chierico, bardo, ladro, barbaro
+//task
+let task = JSON.parse(localStorage.getItem("task")) || [
+    { id: 1, nome: "Studia", fatto: false, xp: 10, xpAssegnato : false, statistica: "intelletto", dataCreazione: "Mon Jan 01 2024" },
+    { id: 2, nome: "Allenati", fatto: false, xp: 10, xpAssegnato : false, statistica: "forza", dataCreazione: "Mon Jan 01 2024" },
+    { id: 3, nome: "Leggi", fatto: false, xp: 10, xpAssegnato : false, statistica: "saggezza", dataCreazione: "Mon Jan 01 2024" },
+    { id: 4, nome: "Suona", fatto: false, xp: 10, xpAssegnato : false, statistica: "carisma", dataCreazione: "Mon Jan 01 2024" },
+    { id: 5, nome: "Pratica", fatto: false, xp: 10, xpAssegnato : false, statistica: "destrezza", dataCreazione: "Mon Jan 01 2024" },
+    { id: 6, nome: "Bere", fatto: false, xp: 10, xpAssegnato : false, statistica: "costituzione", dataCreazione: "Mon Jan 01 2024" }
+];
+
+//stat
+let giocatore = JSON.parse(localStorage.getItem("giocatore")) || {
+    nome: null,
+    xp: 0,
+    livello: 1,
+    classe: null,
+
+    statistiche: {
+        forza: 0,
+        saggezza: 0,
+        intelletto: 0,
+        carisma: 0,
+        destrezza: 0,
+        costituzione: 0
+    }
+};
+
+if (giocatore.nome !== null) {
+    formPersonaggio.hidden = true;
+    scelta2.hidden = false;
+}
+
+if (giocatore.classe !== null) {
+    incipit.hidden = true;
+    home.hidden = false;
+}
+
+const classi = {
+    guerriero: {
+        forza: 8,
+        saggezza: 3,
+        intelletto: 3,
+        carisma: 4,
+        destrezza: 5,
+        costituzione: 7
+    },
+
+    mago: {
+        forza: 2,
+        saggezza: 6,
+        intelletto: 9,
+        carisma: 4,
+        destrezza: 3,
+        costituzione: 3
+    },
+
+    chierico: {
+        forza: 4,
+        saggezza: 9,
+        intelletto: 5,
+        carisma: 6,
+        destrezza: 2,
+        costituzione: 5
+    },
+
+    bardo: {
+        forza: 3,
+        saggezza: 5,
+        intelletto: 5,
+        carisma: 9,
+        destrezza: 6,
+        costituzione: 3
+    },
+
+    ladro: {
+        forza: 4,
+        saggezza: 4,
+        intelletto: 6,
+        carisma: 4,
+        destrezza: 9,
+        costituzione: 4
+    },
+
+    barbaro: {
+        forza: 9,
+        saggezza: 3,
+        intelletto: 2,
+        carisma: 3,
+        destrezza: 5,
+        costituzione: 9
+    }
+};
+
+function scegliClasse(nomeClasse) {
+    giocatore.classe = nomeClasse;
+    giocatore.statistiche = {
+        ...classi[nomeClasse]
+    };
+    salvaGiocatore();
+
+    incipit.hidden = true;
+    home.hidden = false;
+
+    mostraStatRiassunto();
+    mostraStatDettaglio();
+}
+
+//Reset Giornaliero
 function controllaReset() {
     const oggi = new Date().toDateString();
     const ultimoReset = localStorage.getItem("ultimoReset");
 
     if (ultimoReset !== oggi) {
+        const taskDaConsolidare = task.filter(elemento => elemento.xpAssegnato === true);
+        const xpDaConsolidare = taskDaConsolidare.reduce((totale, elemento) => {
+            return totale + elemento.xp;
+        }, 0);
+
+        giocatore.xp += xpDaConsolidare;
+        giocatore.livello = calcolaLivello(giocatore.xp);
+
+        taskDaConsolidare.forEach(elemento => {
+            giocatore.statistiche[elemento.statistica] += elemento.xp;
+        });
+
         task = task.map(elemento => ({
             ...elemento,
-            fatto: false
+            fatto: false,
+            xpAssegnato: false
         }));
 
         localStorage.setItem("ultimoReset", oggi);
         salvaTask();
+        salvaGiocatore();
     }
 }
 
+//mostra task e funzioni su checked, disabled e elimina
 function mostraTask() {
     if (task.length === 0) {
-        listaTask.innerHTML = "<p>Nessuna task ancora. Aggiungine una!</p>";
+        listaTask.innerHTML = "<p>Nessuna quest ancora. Aggiungine una!</p>";
         return;
     }
     const nomiTask = task.map(elemento => {
@@ -51,33 +182,42 @@ function mostraTask() {
     listaTask.innerHTML = nomiTask;
 };
 
+//creazione id task
 listaTask.addEventListener("change", (evento) => {
     const idTask = Number(evento.target.dataset.id);
     const taskCliccato = task.find(elemento => elemento.id === idTask);
 
-    const confermato = confirm(`Confermi di aver completato "${taskCliccato.nome}"?`);
+    const oggi = new Date().toDateString();
+    if (taskCliccato.dataCreazione === oggi) {
+        alert("Hai appena accettato la quest, domani potrai metterti in viaggio per completarla.");
+        evento.target.checked = false;
+        return;
+    }
+
+    //conferma del check
+    const confermato = confirm(`Confermi di aver completato la quest "${taskCliccato.nome}"?`);
     if (!confermato) {
         evento.target.checked = false;
         return;
     }
 
-    giocatore.xp += taskCliccato.xp;
-    giocatore.livello = Math.floor(giocatore.xp / 100) + 1;
 
     task = task.map(elemento => {
     if (elemento.id === idTask) {
         return {
-            ...elemento, fatto: true};
+        ...elemento, fatto: true,
+        xpAssegnato: true };
     }
 
     return elemento;
 });
 mostraTask();
-mostraStat()
+mostraStatRiassunto();
+mostraStatDettaglio();
 salvaTask();
 salvaGiocatore();
 });
-
+//Rimozione task
 listaTask.addEventListener ("click", (evento) => {
     if (!evento.target.classList.contains("rimuovi")) {
         return;
@@ -90,6 +230,20 @@ listaTask.addEventListener ("click", (evento) => {
     salvaTask();
 });
 
+sceltaClasse.addEventListener("click", (evento) => {
+    if (!evento.target.dataset.classe) {
+        return;
+    }
+
+    const nomeClasse = evento.target.dataset.classe;
+    const decisione = confirm(`Mi assicuri che la tua via sia quella del "${nomeClasse}"?`)
+    if(!decisione) {
+        return;
+    }
+    scegliClasse(nomeClasse);
+});
+
+
 bottoneAggiungi.addEventListener("click", () => {
     //Reindirizzare nuovotask in una macrocategoria a seconda delle caratteristiche
      //magari usando un dropdown per caratteristica nella creazione della nuova task
@@ -97,11 +251,19 @@ bottoneAggiungi.addEventListener("click", () => {
         return;
     }
 
+    const limite = calcolaLimiteTask();
+    if (task.length >= limite) {
+        alert (`Non affaticarti troppo, ${limite} quest vanno più che bene per il tuo livello attuale.`);
+        return;
+    }
+
     const nuovoTask = {
     id: Date.now(),
     nome: inputTask.value.trim(),
     fatto: false,
-    xp: 10
+    xp: 10,
+    xpAssegnato: false,
+    dataCreazione: new Date().toDateString()
 };
 
 
@@ -111,9 +273,52 @@ mostraTask();
 salvaTask();
 });
 
- function mostraStat() {
-       stat.innerHTML = `<p>livello: ${giocatore.livello} | XP: ${giocatore.xp}</p>`
-};
+tabBar.addEventListener("click", (evento) => {
+    if (!evento.target.dataset.pagina) {
+        return;
+    }
+
+    paginaQuest.hidden = true;
+    paginaStatistiche.hidden = true;
+
+    if (evento.target.dataset.pagina === "pagina-quest") {
+        paginaQuest.hidden = false;
+    } else {
+        paginaStatistiche.hidden = false;
+    }
+});
+
+//creazione task
+function mostraStatRiassunto() {
+    const taskDiOggi = task.filter(elemento => elemento.xpAssegnato === true);
+    const xpProvvisorio = taskDiOggi.reduce((totale, elemento) => totale + elemento.xp, 0);
+    const xpVisualizzato = giocatore.xp + xpProvvisorio;
+    const livelloVisualizzato = calcolaLivello(xpVisualizzato);
+
+    document.querySelector("#stat-riassunto").innerHTML =
+        `<p>${giocatore.nome} - livello: ${livelloVisualizzato} | XP: ${xpVisualizzato}</p>`;
+}
+
+function mostraStatDettaglio() {
+    const taskDiOggi = task.filter(elemento => elemento.xpAssegnato === true);
+    const xpProvvisorio = taskDiOggi.reduce((totale, elemento) => totale + elemento.xp, 0);
+    const xpVisualizzato = giocatore.xp + xpProvvisorio;
+    
+    let livelloVisualizzato = 1;
+    let xpAccumulata = 0;
+    while (xpAccumulata + xpNecessaria(livelloVisualizzato) <= xpVisualizzato) {
+        xpAccumulata += xpNecessaria(livelloVisualizzato);
+        livelloVisualizzato++;
+    }
+    const xpNelLivello = xpVisualizzato - xpAccumulata;
+
+    const listaStatistiche = Object.entries(giocatore.statistiche)
+        .map(([nome, valore]) => `${nome}: ${valore}`)
+        .join(" | ");
+
+    document.querySelector("#stat-dettaglio").innerHTML = `<p>${giocatore.nome} - livello: ${livelloVisualizzato} | Prossimo livello: ${xpNelLivello}/${xpNecessaria(livelloVisualizzato)} XP <br><br> ${listaStatistiche}</p>`;
+}
+
 
 function salvaTask () {
     localStorage.setItem("task", JSON.stringify(task));
@@ -123,6 +328,27 @@ function salvaGiocatore() {
     localStorage.setItem("giocatore", JSON.stringify(giocatore));
 }
 
+function calcolaLimiteTask () {
+    const limite = 10 + Math.floor ((giocatore.livello - 1) / 5);
+    return Math.min (limite, 30);
+}
+
+function xpNecessaria(livello) {
+    return livello * 20;
+}
+
+function calcolaLivello(xpTotale) {
+    let livello = 1;
+    let xpAccumulata = 0;
+
+    while (xpAccumulata + xpNecessaria(livello) <= xpTotale) {
+        xpAccumulata += xpNecessaria(livello);
+        livello++;
+    }
+    return livello;
+}
+
 controllaReset();
-mostraStat();
+mostraStatRiassunto();
+mostraStatDettaglio();
 mostraTask();
